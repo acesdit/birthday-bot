cb79886b5de20f7fb28af94200e1797f0b59b585
sahil-s-246 <sahilsrinivas3@gmail.com>
2024-08-15 14:49:58 +0530

    vercel hello world

--- index.py
+++ index.py
@@ -3,19 +3,19 @@ from slack_bolt.adapter.flask import SlackRequestHandler
 from message_counter import slack_app
 from waitress import serve
 app = Flask(__name__)
-handler = SlackRequestHandler(slack_app)
+# handler = SlackRequestHandler(slack_app)
 
 
-@app.route("/" ,methods=["GET", "POST"])
+@app.route("/" , methods=["GET", "POST"])
 def main():
     return "Hello World!"
 
-
-@app.route("/slack/events", methods=["POST"])
-def slack_events():
-    if request.content_type == "application/json":
-        return {"challenge": request.json["challenge"]}
-    return handler.handle(request)
+#
+# @app.route("/slack/events", methods=["POST"])
+# def slack_events():
+#     if request.content_type == "application/json":
+#         return {"challenge": request.json["challenge"]}
+#     return handler.handle(request)
 
 
 if __name__ == "__main__":
